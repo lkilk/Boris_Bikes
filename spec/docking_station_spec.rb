865f591ba0fe014987edf39d7fae6b3e42273bc9
Liam Kilkenny <81643363+lkilk@users.noreply.github.com>
2021-04-27 17:42:01 +0100

    update

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,14 +1,32 @@
-require 'docking_station'
-require 'bike'
+require 'docking_station.rb'
 
 describe DockingStation do
-  it "Docking Station responds to release bike" do
-    expect(subject).to respond_to :release_bike
+  it "docking station responds to release bike" do
+  expect(subject).to respond_to :release_bike
   end
 
-  it "Releases working bike is working" do
+  it "releases bike and checks bike is working" do
     bike = subject.release_bike
     expect(bike).to be_working
+  end
+
+  it "allows a bike to be docked" do
+    expect(subject).to respond_to(:dock).with(1).argument
+  end
+
+  it "allows you to view bikes" do
+    expect(subject).to respond_to :bike
+  end
+
+  it "docks a bike" do
+    bike = Bike.new
+    expect(subject.dock(bike)).to eq(bike)
+  end
+
+  it "returns if a bike is docked" do
+    bike = Bike.new
+    subject.dock(bike)
+    expect(subject.bike).to eq(bike)
   end
 
 end
