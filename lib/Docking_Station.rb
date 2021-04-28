c0be1b97cbd15ed9e96db0a38bc99086dbaf50a1
Liam Kilkenny <81643363+lkilk@users.noreply.github.com>
2021-04-28 16:22:09 +0100

    update

--- lib/Docking_Station.rb
+++ lib/Docking_Station.rb
@@ -1,14 +1,34 @@
 require_relative './bike.rb'
 
 class DockingStation
-  attr_reader :bike
+  DEFAULT_CAPACITY = 20
+  attr_reader :capacity
+
+  def initialize(capacity=DEFAULT_CAPACITY)
+    @bikes = []
+    @capacity = capacity
+  end
 
   def release_bike
-    Bike.new
+    fail "No bikes available" if empty?
+    @bikes.pop
   end
 
   def dock(bike)
-    @bike = bike
+    fail 'Docking station full' if full?
+    @bikes << bike
+  end
+
+  attr_reader :bike
+
+  private
+
+  def full?
+    @bikes.count >= capacity
+  end
+
+  def empty?
+    @bikes.empty?
   end
 
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,13 +1,8 @@
-require 'docking_station.rb'
+require 'Docking_Station.rb'
 
 describe DockingStation do
   it "docking station responds to release bike" do
   expect(subject).to respond_to :release_bike
-  end
-
-  it "releases bike and checks bike is working" do
-    bike = subject.release_bike
-    expect(bike).to be_working
   end
 
   it "allows a bike to be docked" do
@@ -20,13 +15,64 @@ describe DockingStation do
 
   it "docks a bike" do
     bike = Bike.new
-    expect(subject.dock(bike)).to eq(bike)
+    expect(subject.dock(bike)).to eq([bike])
   end
 
-  it "returns if a bike is docked" do
-    bike = Bike.new
-    subject.dock(bike)
-    expect(subject.bike).to eq(bike)
+
+  describe '#release_bike' do # This bit ere makes the bike equal to the bike your docking, weird
+    it 'releases a bike' do
+      bike = Bike.new
+      subject.dock(bike)
+      expect(subject.release_bike).to eq bike
+    end
   end
 
+  describe '#release_bike' do # then this bit ere raises an error if no bikes are available
+    it 'raises error when no bikes are there' do
+      expect { subject.release_bike }.to raise_error 'No bikes available'
+    end
+  end
+
+  describe '#dock' do # then this bit ere raises an error if no bikes are available
+    it 'raises error when docking station is full' do
+    subject.capacity.times { subject.dock Bike.new }
+      expect { subject.dock Bike.new }.to raise_error 'Docking station full'
+    end
+  end
+
+  it "check to see if a default capacity has been set" do
+    expect(subject.capacity).to eq DockingStation::DEFAULT_CAPACITY
+  end
+
+  it "allows user to set capacity of new docking station class" do
+    docking_station = DockingStation.new(25)
+    expect(docking_station.capacity).to eq(25)
+  end
+
+  describe 'initialization' do
+    it 'has a variable capacity' do
+      docking_station = DockingStation.new(50)
+      50.times { docking_station.dock Bike.new }
+      expect{ docking_station.dock Bike.new }.to raise_error 'Docking station full'
+    end
+  end
+
+  describe 'initialization' do
+    it 'has a default capacity' do
+      docking_station = DockingStation.new
+      DockingStation::DEFAULT_CAPACITY.times { docking_station.dock Bike.new }
+      expect{ docking_station.dock Bike.new }.to raise_error 'Docking station full'
+    end
+  end
+
+  # describe "initialization" do
+  #   subject { DockingStation.new }
+  #   let(:bike) { Bike.new }
+  #   it 'defaults capacity' do
+  #     described_class::DEFAULT_CAPACITY.times do
+  #       subject.dock(bike)
+  #   end
+  #   expect{ subject.dock(bike) }.to raise_error 'Docking station full'
+  # end
+  # end
 end
